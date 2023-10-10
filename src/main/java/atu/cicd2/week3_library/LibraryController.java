e2bfbc2c1543f0e96245dc40be7fa9b079b0ac8c
Allyn McKenna Patterson <allyncodes@gmail.com>
2023-10-10 18:59:15 +0100

    Added PostMapping

--- src/main/java/atu/cicd2/week3_library/LibraryController.java
+++ src/main/java/atu/cicd2/week3_library/LibraryController.java
@@ -1,4 +1,20 @@
 package atu.cicd2.week3_library;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class LibraryController {
+    private LibraryService myLibraryService;
+
+
+
+    @PostMapping("library")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Book registerBook(@RequestBody Book book) {
+
+    }
 }
