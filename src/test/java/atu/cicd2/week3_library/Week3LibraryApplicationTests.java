5d617785cdcde2ee530aec98dd91701c86cec40f
Allyn McKenna Patterson <allyncodes@gmail.com>
2023-10-10 18:21:33 +0100

    Initial commit

---
+++ src/test/java/atu/cicd2/week3_library/Week3LibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package atu.cicd2.week3_library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week3LibraryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
