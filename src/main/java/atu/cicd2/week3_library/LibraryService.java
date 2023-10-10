d759b56a43e645fb0902e9c8339664b6050d30b5
Allyn McKenna Patterson <allyncodes@gmail.com>
2023-10-10 18:53:44 +0100

    Added addBook() and getBooks() methods

--- src/main/java/atu/cicd2/week3_library/LibraryService.java
+++ src/main/java/atu/cicd2/week3_library/LibraryService.java
@@ -1,4 +1,25 @@
 package atu.cicd2.week3_library;
 
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+
+@Service
 public class LibraryService {
+    private ArrayList <Book> books;
+
+    public LibraryService(){
+        books = new ArrayList<Book> ();
+    }
+    public void addBook(Book book){
+        books.add(book);
+        System.out.println(book.getTitle() + " added to library");
+    }
+
+    public void getBooks(){
+        for(Book book: books)
+            System.out.println(book);
+    }
+
+
 }
