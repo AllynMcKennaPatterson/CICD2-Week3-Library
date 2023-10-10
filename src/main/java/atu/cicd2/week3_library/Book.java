05fd861fb38695c611c7b711d0019d627a5064f2
Allyn McKenna Patterson <allyncodes@gmail.com>
2023-10-10 18:39:11 +0100

    Added Book attributes

--- src/main/java/atu/cicd2/week3_library/Book.java
+++ src/main/java/atu/cicd2/week3_library/Book.java
@@ -1,4 +1,15 @@
 package atu.cicd2.week3_library;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Book {
+    String title;
+    String author;
+    String isbn;
+    int publishedYear;
 }
